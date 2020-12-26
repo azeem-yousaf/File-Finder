db9057074bb6b3bc2fee8784d10f3fdd29510716
azeem-yousaf <azeem251194@gmail.com>
2020-12-26 14:34:59 +0000

    First commit
    Program that find a file which contains a word you inputted.

---
+++ File Finder/File Finder/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace File_Finder
+{
+    class Program
+    {
+        private static string fileName;
+        /// <summary>
+        /// This program allows the user to find a file within their pc
+        /// </summary>
+        /// <param name="args">[0] directory to look in (use / to search entire pc), [1] file name</param>
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Try typing finder <directory> <filename>. If you enter / for directory then it will search all drives.");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                return;
+            }
+            string directory = args[0];
+            fileName = args[1];
+            bool searchEntireSystem = directory == "/" ? true : false;
+
+            if (searchEntireSystem)
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (var drive in drives)
+                {
+                    var folders = drive.RootDirectory;
+                    FindFiles(folders);
+
+                }
+            }
+            else
+            {
+                var dir = new DirectoryInfo(directory);
+                FindFiles(dir);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        private static void FindFiles(DirectoryInfo folder)
+        {
+            try
+            {
+                foreach (var file in folder.GetFiles())
+                {
+                    if (file.Name.ToLower().Contains(fileName.ToLower())) PrintMatchFound(file.FullName);
+
+                }
+                foreach (var f in folder.GetDirectories())
+                {
+                    FindFiles(f);
+                }
+            }
+            catch
+            {
+            }
+        }
+        private static void PrintMatchFound(string file)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(file);
+        }
+    }
+}
